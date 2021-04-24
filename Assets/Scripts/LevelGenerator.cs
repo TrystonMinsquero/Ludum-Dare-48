332335467a2647f0befae541ff4d7a9e2b147bc3
Tryston Minsquero <tam190005@utdallas.edu>
2021-04-24 17:57:14 -0500

    Some shit

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,79 +5,126 @@ using System;
 
 public class LevelGenerator : MonoBehaviour
 {
+    public static LevelGenerator instance;
+
     public int numberOfDifficulties = 10;
+    public GameObject obstacle_Folder;
+    public GameObject obstacle_Prefab;
+
+    private static GameObject obstacleFolder;
+    private static GameObject obstaclePrefab;
+
     public static List<Texture2D[]> obstacleMaps;
     public static Texture2D[] foregroundMaps;
     public static Texture2D[] backgroundMaps;
-    public ColorToPrefab[] colorMappings;
 
+    public static List<TileBase[]> obstacleTiles;
+    public static List<TileBase[]> environmentTiles;
 
+    public Tilemap[] tile_Maps;
 
+    private static Tilemap[] tileMaps;
 
-    enum TileMapType
+    private void Awake()
     {
-        FOREGROUND,
-        FEEDTAPE,
-        BACKGROUND
+        if (instance != null)
+            Destroy(this.gameObject);
+        else
+            instance = this;
     }
-
-    public Tilemap[] tileMaps;
 
     void Start()
     {
         obstacleMaps = new List<Texture2D[]>();
+        obstacleTiles = new List<TileBase[]>();
+        environmentTiles = new List<TileBase[]>();
+        obstacleFolder = obstacle_Folder;
+        obstaclePrefab = obstacle_Prefab;
+        tileMaps = tile_Maps;
+
 
         for( int i = 0; i < numberOfDifficulties; i++)
         {
             obstacleMaps.Add(Resources.LoadAll<Texture2D>("Segments/Obstacles/Difficulty_" + i));
+            //Debug.Log("Obstacle maps of diff " + i + " loaded " + obstacleMaps[i].Length + " maps");
         }
 
         foregroundMaps = Resources.LoadAll<Texture2D>("Segments/Foreground");
+        //Debug.Log("Foreground maps loaded " + foregroundMaps.Length + " maps");
         backgroundMaps = Resources.LoadAll<Texture2D>("Segments/Background");
+        //Debug.Log("Background maps loaded " + backgroundMaps.Length + " maps");
 
-        GenerateTile(backgroundMaps[0], 0, 0, TileMapType.BACKGROUND);
+        for(int i = (int)LevelSection.GROUND; i < (int)LevelSection.BOTTOM; i++)
+        {
+            obstacleTiles.Add(null);
+            environmentTiles.Add(null);
+        }
+        obstacleTiles[(int)LevelSection.CRUST] = Resources.LoadAll<TileBase>("TileMaps/Crust/Obstacles/Tiles");
+        //obstacleTiles[(int)LevelSection.MANTLE] = Resources.LoadAll<TileBase>("TileMaps/Mantle/Obstacles/Tiles");
+        //obstacleTiles[(int)LevelSection.CORE] = Resources.LoadAll<TileBase>("TileMaps/Core/Obstacles/Tiles");
+
+        environmentTiles[(int)LevelSection.CRUST] = Resources.LoadAll<TileBase>("TileMaps/Crust/Environment/Tiles");
+        //environmentTiles[(int)LevelSection.MANTLE] = Resources.LoadAll<TileBase>("TileMaps/Mantle/Environment/Tiles");
+        //environmentTiles[(int)LevelSection.CORE] = Resources.LoadAll<TileBase>("TileMaps/Core/Environment/Tiles");
+
+        GenerateSegment();
         //GenerateLevel();
     }
 
-    void GenerateLevel()
+    public static void GenerateSegment()
     {
-        
+        GenerateBackground(backgroundMaps[0]);
+        GenerateFeedTape(foregroundMaps[0]);
+        GenerateForeground(obstacleMaps[0][0]);
     }
 
-    private void GenerateForeground(Texture2D segment)
+    private static void GenerateForeground(Texture2D segment)
     {
         for (int x = 0; x < segment.width; x++)
         {
             for (int y = 0; y < segment.height; y++)
             {
-                GenerateTile(segment, x, y);
+                GenerateTile(segment, x, y, TileMapType.FOREGROUND);
             }
         }
 
     }
 
-    private void GenerateFeedTape(Texture2D segment)
+    private static void GenerateFeedTape(Texture2D segment)
     {
-        
+        for (int x = 0; x < segment.width; x++)
+        {
+            for (int y = 0; y < segment.height; y++)
+            {
+                GenerateTile(segment, x, y, TileMapType.FEEDTAPE);
+            }
+        }
     }
 
-    private void GenerateBackground(Texture2D segment)
+    private static void GenerateBackground(Texture2D segment)
     {
-
+        for (int x = 0; x < segment.width; x++)
+        {
+            for (int y = 0; y < segment.height; y++)
+            {
+                GenerateTile(segment, x, y, TileMapType.BACKGROUND);
+            }
+        }
     }
 
-    void GenerateTile(Texture2D map, int x, int y, TileMapType tilemap = TileMapType.FOREGROUND)
+    public static void GenerateTile(Texture2D map, int x, int y, TileMapType tileMapType)
     {
         Color pixelColor = map.GetPixel(x, y);
-        
-        Vector2 pos = tilemap == TileMapType.FOREGROUND ? 
+        Tilemap tileMap = tileMaps[(int)tileMapType];
+
+        Vector2 pos = tileMapType == TileMapType.FOREGROUND ? 
             (Vector2)(LevelManager.cam.transform.position) - Vector2.up * 4.5f - Vector2.left * 5.5f
             : (Vector2)(LevelManager.cam.transform.position) - Vector2.up * 4.5f - Vector2.left * 6.5f;
 
         pos -= Vector2.down * 20;
         pos.x += x;
         pos.y += y;
-        Vector3Int worldPos = tileMaps[(int)tilemap].WorldToCell(pos);
+        Vector3Int gridPos = tileMap.WorldToCell(pos);
 
         if (pixelColor.a == 0)
         { return; }
@@ -85,6 +132,17 @@ public class LevelGenerator : MonoBehaviour
         ColorUtility.ToHtmlStringRGB(pixelColor);
         int tileIndex = int.Parse(ColorUtility.ToHtmlStringRGB(pixelColor), System.Globalization.NumberStyles.HexNumber);
         Debug.Log("(" + x + ", " + y + ") = " + tileIndex);
+
+
+        if(tileMapType == TileMapType.FOREGROUND)
+        {
+            tileMap.SetTile(gridPos, obstacleTiles[(int)LevelManager.levelSection][tileIndex]);
+            Instantiate(obstaclePrefab, obstacleFolder.transform).transform.position = pos;
+        }
+        else
+        {
+            tileMap.SetTile(gridPos, environmentTiles[(int)LevelManager.levelSection+1][tileIndex]);
+        }
 
         /*
         foreach (ColorToPrefab colorMapping in colorMappings)
@@ -103,15 +161,13 @@ public class LevelGenerator : MonoBehaviour
         */
     }
 
-    public static string ToRGBHex(Color c)
+    public enum TileMapType
     {
-        return string.Format("#{0:X2}{1:X2}{2:X2}", ToByte(c.r), ToByte(c.g), ToByte(c.b));
+        FOREGROUND,
+        FEEDTAPE,
+        BACKGROUND
     }
-
-    private static byte ToByte(float f)
-    {
-        f = Mathf.Clamp01(f);
-        return (byte)(f * 255);
-    }
-
 }
+
+
+
