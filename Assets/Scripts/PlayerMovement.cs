c97347b3153cd0fd39b665602c099052e23d82a2
Tryston Minsquero <tam190005@utdallas.edu>
2021-04-23 22:21:56 -0500

    place holders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
 
     bool facingRight;
+    bool grounded = true;
     bool falling;
 
     // Start is called before the first frame update
@@ -19,23 +20,49 @@ public class PlayerMovement : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
- 
-    }
-
     private void FixedUpdate()
     {
+
         Vector2 playerMovement = controls.Gameplay.Movement.ReadValue<Vector2>();
-        if (playerMovement != Vector2.zero)
+
+        if (falling)
         {
-            rb.velocity = playerMovement * moveSpeed;
-            if (playerMovement.x != 0)
-                facingRight = playerMovement.x == 1 ? true : false;
+            rb.gravityScale = 0;
+            //Directional movement
+            if (playerMovement != Vector2.zero)
+            {
+                rb.velocity = playerMovement * moveSpeed;
+                if (playerMovement.x != 0)
+                {
+                    facingRight = playerMovement.x == 1 ? true : false;
+                    transform.localScale = new Vector3(playerMovement.x, 1, 1);
+                }
+            }
+            else
+                rb.velocity = Vector2.zero;
         }
         else
-            rb.velocity = Vector2.zero;
+        {
+            rb.gravityScale = 1;
+            if (playerMovement.x != 0)
+            {
+                rb.velocity = new Vector2(playerMovement.x * moveSpeed, rb.velocity.y);
+            }
+            else if (grounded)
+            {
+                rb.velocity = Vector2.zero;
+            }
+
+            if (playerMovement.y > 0 && grounded)
+            {
+                grounded = false;
+                rb.AddForce(new Vector2(0, moveSpeed * 2));
+            }
+
+        }
+        
+
+
+
     }
 }
