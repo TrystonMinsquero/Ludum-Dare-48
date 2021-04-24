8d31ec9005a428d702695cae4c83af62937eecbf
Michbet <68103256+Michbet@users.noreply.github.com>
2021-04-24 00:04:52 -0500

    Gem Prefabs

---
+++ Assets/Scripts/GemScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScript : MonoBehaviour
+{
+    public int valueOfGem;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        //add valueOfGem to players $$$
+    }
+  
+}
